f8b9ce804970dbff74273436ac4cf96f530c97dd
raszpberry <golimlim2209@gmail.com>
2022-11-07 12:53:03 +0800

    initialcommit

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-console.log("Hi");
-
-let img = document.querySelector("img");
+let imgSource = document.querySelector("img");
 let prev = document.querySelector("#go-to-previous");
 let next = document.querySelector("#go-to-next");
-
 let i = 0;
+
 next.addEventListener("click", function () {
-  img.setAttribute("src", `https://picsum.photos/id/${i++}/500/300`);
-  console.log(img.getAttribute("src"));
+  imgSource.setAttribute("src", `https://picsum.photos/id/${i++}/500/300`);
+  console.log(imgSource.getAttribute("src"));
 });
+
 prev.addEventListener("click", function () {
-  img.setAttribute("src", `https://picsum.photos/id/${i--}/500/300`);
-  console.log(img.getAttribute("src"));
+  imgSource.setAttribute("src", `https://picsum.photos/id/${i--}/500/300`);
+  console.log(imgSource.getAttribute("src"));
+  if (i < 0) {
+    i = 0;
+  }
 });
